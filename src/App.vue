ba079b089d33c4d7a3463583c009a1bdeb519590
CodHan <qpscm662@naver.com>
2024-08-27 00:23:43 +0900

    props 사용 방법 (Options와 Compositon )

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,22 @@
 <template>
-  <div class="wrapper">
-    <div v-for="item in otherArray" :key="item.id">
-      <h1>{{ item.name }}</h1>
-    </div>
+  <div>
+    <ChildComponent :props1="title" :props2="createAt" :props3="obj" />
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      sampleArray: ["a", "b", "c", "d", "e", "f", "g"],
-      otherArray: [
-        {
-          id: 0,
-          name: "han",
-        },
-        {
-          id: 1,
-          name: "eun",
-        },
-        {
-          id: 2,
-          name: "beom",
-        },
-      ],
-    };
-  },
-};
+<script setup lang="ts">
+//setup 선언
+import { reactive, ref } from "vue"; //import
+import ChildComponent from "./components/ChildComponent.vue";
+
+interface Obj {
+  id: number;
+  name: string;
+}
+
+const title = ref<string>("APP에서 생성된 데이터"); //ref를 통해 state 선언
+const createAt = ref<number>(2024);
+const obj = reactive<Obj>({ id: 2024, name: "한은범" }); // !!객체는 ref보단 reactive를 사용하는 것을 권장!!
 </script>
 
-<style scoped>
-.wrapper {
-  display: flex;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>{{ props1 }}</div>
+  <div>{{ props2 }}</div>
+  <div>{{ props3.id }}</div>
+  <div>{{ props3.name }}</div>
+</template>
+
+<script setup lang="ts">
+// 동일하게 setup과 lang 설정
+
+import { toRefs } from "vue";
+
+interface Obj {
+  id: number;
+  name: string;
+}
+
+interface Props {
+  props1: String;
+  props2: Number;
+  props3: Obj;
+}
+
+const props = defineProps<Props>(); // 방법1. defineProps 내장함수 사용
+const { props1, props2, props3 } = toRefs(props); // 방법2. 구조분해 할당으로 받아올 경우 toRefs 내장함수 사용 import 해야함!!
+</script>
+
+<style lang="scss" scoped></style>
