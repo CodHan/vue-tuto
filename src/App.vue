504f4fbd62277dc7f13ac87dda234024d4e2e09e
CodHan <qpscm662@naver.com>
2024-08-26 19:07:36 +0900

    v-for문 연습

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-for="(item, idx) in sampleArray" :key="idx">
+    <h1>
+      {{ item }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sampleArray: ["a", "b", "c", "d", "e", "f", "g"],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
